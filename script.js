7f0b5c5ee708a5f1c2245dd996625823c2e0143e
Daniel Yu <daniel.sm.yu@gmail.com>
2018-08-23 12:57:25 +0200

    created array for pixels

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+class pixel {
+	constructor(date, emotion, rating, high, low) {
+		this.date = date;
+		this.emotion = emotion;
+		this.rating = rating;
+		this.high = high;
+		this.low = low;
+	}	
+};
+
+var pixelSheet = [];
+
+for (var i=0; i<62; i+=1) {
+	var date = i + 1;
+	var emotion = Math.floor(Math.random()*8) + 1;
+	var rating = Math.floor(Math.random()*10) + 1;
+	var high = Math.floor(Math.random()*6) + 1;
+	var low = Math.floor(Math.random()*6) + 1;
+	
+	pixelSheet[i] = new pixel(date, emotion, rating, high, low);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
 // var slider = document.getElementById("myRange");
 // var output = document.getElementById("demo");
 // output.innerHTML = slider.value; // Display the default slider value
